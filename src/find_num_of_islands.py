b2e40a17563eb21fe962865ebdb30bcd1a1e76f2
VilenSolovey <solovey.vilen21@gmail.com>
2024-04-08 19:52:07 +0300

    Changed Lab5
    Replaced cell_neighbours with a constant NEIGHBORS, create new function. In tests made made the method "read_output_txt_files" static. In general made the code more readable

--- src/find_num_of_islands.py
+++ src/find_num_of_islands.py
@@ -1,5 +1,6 @@
 """Lab 5"""
 from collections import deque
+NEIGHBORS = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
 
 def count_number_of_islands(input_file_path,output_file_path):
     grid = []
@@ -23,19 +24,21 @@ def count_number_of_islands(input_file_path,output_file_path):
         file.write(str(amount_of_islands))
 
     return amount_of_islands
+def is_valid_move(next_row, next_col, grid, visited, num_rows, num_cols):
+    return (0 <= next_col < num_cols and
+            0 <= next_row < num_rows and
+            grid[next_row][next_col] == 1 and
+            (next_row, next_col) not in visited)
 def bfs(current_row, current_col, grid, visited, num_rows, num_cols):
     queue_for_bfs = deque()
     queue_for_bfs.append((current_row, current_col))
     visited.add((current_row, current_col))
 
-    cell_neighbours = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
     while queue_for_bfs:
         current_row, current_col = queue_for_bfs.popleft()
 
-        for row_id, col_id in cell_neighbours:
-            if (0 <= current_col + col_id < num_cols and
-                    0 <= current_row + row_id < num_rows and
-                    grid[current_row + row_id][current_col + col_id] == 1 and
-                    (current_row + row_id, current_col + col_id) not in visited):
-                visited.add((current_row + row_id, current_col + col_id))
-                queue_for_bfs.append((current_row + row_id, current_col + col_id))
+        for row_id, col_id in NEIGHBORS:
+            next_row, next_col = current_row + row_id, current_col + col_id
+            if is_valid_move(next_row, next_col, grid, visited, num_rows, num_cols):
+                visited.add((next_row, next_col))
+                queue_for_bfs.append((next_row, next_col))
