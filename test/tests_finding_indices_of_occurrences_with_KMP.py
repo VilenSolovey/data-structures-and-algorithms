7d7b043c438e12e84febb1cd9361b6578102d87b
VilenSolovey <solovey.vilen21@gmail.com>
2024-04-26 10:03:58 +0300

    Completed Lab7

---
+++ test/tests_finding_indices_of_occurrences_with_KMP.py
@@ -0,0 +1,17 @@
+import unittest
+from finding_indices_of_occurrences_with_KMP import knuth_morris_pratt
+
+
+class MyTestCase(unittest.TestCase):
+    def test_lab7_case1(self):
+        self.assertEqual(knuth_morris_pratt('CD', 'CDCDCDCD'), {0, 2, 4, 6})
+
+    def test_lab7_case2(self):
+        self.assertEqual(knuth_morris_pratt('Vilen', 'Vilen in the London, Vilen'), {0, 21})
+
+    def test_lab7_case3(self):
+        self.assertEqual(knuth_morris_pratt('ADC', 'BADCDADC'), {1, 5})
+
+
+if __name__ == '__main__':
+    unittest.main()
